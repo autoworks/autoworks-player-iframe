6736f3f3d6671da272b14f5629d209a0c909c96b
rymate1234 <ryan.murthick@gmail.com>
2020-10-22 11:26:59 +0100

    feat: Deploy IFrame as a separate site

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import 'autoworks-player/dist/AutoWorksPlayer.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,72 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from 'react'
 
-export default function Home() {
+import Player from 'autoworks-player'
+
+const AutoWorksPlayerIFrame = () => {
+  const [options, setOptions] = useState()
+
+  const sendMessage = (type, msg) => {
+    parent.postMessage(
+      {
+        type,
+        ...msg
+      },
+      '*'
+    )
+  }
+
+  const onMessage = (event) => {
+    setOptions({
+      ...event.data,
+      onNavigation: (args) => {
+        sendMessage('onNavigation', args)
+      },
+      onShowcaseEnter: () => {
+        sendMessage('onShowcaseEnter', {})
+      },
+      onShowcaseExit: () => {
+        sendMessage('onShowcaseExit', {})
+      },
+      onVideoProgress: (args) => {
+        sendMessage('onVideoProgress', args)
+      }
+    })
+  }
+
+  const getDocHeight = () => {
+    const body = document.body
+
+    return Math.max(body.scrollHeight, body.offsetHeight)
+  }
+
+  const onResize = () => {
+    sendMessage('resize', {
+      height: getDocHeight()
+    })
+  }
+
+  useEffect(() => {
+    sendMessage('ready', {
+      height: getDocHeight()
+    })
+
+    window.addEventListener('message', onMessage, false)
+    window.addEventListener('resize', onResize, false)
+
+    return () => {
+      window.removeEventListener('message', onMessage)
+    }
+  }, [])
+
+  if (!options) {
+    return <div />
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <Player {...options} />
     </div>
   )
 }
+
+export default AutoWorksPlayerIFrame
