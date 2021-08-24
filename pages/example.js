4e0dee72734eb42a9433473d6e566fa0dc9a334d
rymate1234 <ryan.murthick@gmail.com>
2021-08-24 21:10:13 +0100

    feat: Adds iframe demo

--- pages/example.js
+++ pages/example.js
@@ -1,15 +1,39 @@
-import React from "react";
-import AutoWorksPlayer from "autoworks-player";
+import React, { useEffect } from 'react'
 
-import config from "../fixtures/config";
-import items from "../fixtures/items";
+import config from '../fixtures/config'
+import items from '../fixtures/items'
 
-const options = {
-  items,
-  ...config,
-};
+const options = JSON.parse(
+  JSON.stringify({
+    items,
+    ...config
+  })
+)
 
-export default function Player() {
-  return <AutoWorksPlayer {...options} />;
+export default function IFrameDemo() {
+  useEffect(() => {
+    const iframe = document.getElementById('demo')
+
+    function onMsg(e) {
+      if (!e.data.type) return
+      if (e.data.type === 'ready') {
+        iframe.contentWindow.postMessage(options, '*')
+      }
+
+      iframe.height = e.data.height
+    }
+
+    window.addEventListener('message', onMsg)
+
+    return () => {
+      window.removeEventListener('message', onMsg)
+    }
+  }, [])
+
+  return (
+    <>
+      <h1>IFrame Embed Demo</h1>
+      <iframe style={{border: 0}} id="demo" width="100%" height={600} src="/" title="demo" />
+    </>
+  )
 }
-
